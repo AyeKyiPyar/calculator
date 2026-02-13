eb40e43c5236d58a3de582acb2c30c98f7c40682
AyeKyiPyar <49303336+AyeKyiPyar@users.noreply.github.com>
2026-02-13 14:24:40 +0630

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
+
+SERVER=$1
+
 echo "Running acceptance test..."
-CALCULATOR_PORT=$(ssh -o StrictHostKeychecking=no ubuntu@$@ "docker-compose port calculator 8080 | cut -d: -f2")
-echo "Host: $@:$CALCULATOR_PORT"
-./gradlew acceptanceTest -Dcalculator.url=http://$@:$CALCULATOR_PORT
+
+PORT=$(ssh -o StrictHostKeyChecking=no ubuntu@$SERVER \
+"docker-compose port calculator 8080 | cut -d: -f2")
+
+URL="http://$SERVER:$PORT"
+
+echo "Testing URL: $URL"
+
+mvn verify -Dcalculator.url=$URL
+$CALCULATOR_PORT
